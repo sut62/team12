e64e5bb90d6eade1f859270fd95ea7aed6198ed6
YeenPrawee <yeenprawee@gmail.com>
2020-01-08 01:24:15 +0700

    close #53 - add Repository ของระบบจ่ายยา

---
+++ backend/src/main/java/com/cpe/backend/repository/DrugDispenseRepository.java
@@ -0,0 +1,12 @@
+package com.cpe.backend.repository;
+
+import com.cpe.backend.entity.DrugDispense;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public
+interface DrugDispenseRepository extends JpaRepository<DrugDispense, Long> {
+	DrugDispense findById(long id);
+}
