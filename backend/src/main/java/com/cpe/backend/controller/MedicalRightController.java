de4ef6cec5d5c6d4f122c29425a5ecc0e9a64cb1
sscarry2 <earth.narongsak@gmail.com>
2020-01-08 14:04:28 +0000

    close #22 - add Controller Class ของระบบบันทึกใบเสร็จค่ายา

---
+++ backend/src/main/java/com/cpe/backend/controller/MedicalRightController.java
@@ -0,0 +1,30 @@
+package com.cpe.backend.controller;
+
+import com.cpe.backend.Entity.MedicalRight;
+import com.cpe.backend.repository.MedicalRightRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+public class MedicalRightController {
+
+    @Autowired
+    private final MedicalRightRepository medicalRightRepository;
+
+    public MedicalRightController(MedicalRightRepository medicalRightRepository) {
+        this.medicalRightRepository = medicalRightRepository;
+    }
+
+    @GetMapping("/medicalRight")
+    public Collection<MedicalRight> medicalRights() {
+        return medicalRightRepository.findAll().stream().collect(Collectors.toList());
+    }
+
+}
---
+++ backend/src/main/java/com/cpe/backend/controller/PaymentChannelController.java
@@ -0,0 +1,30 @@
+package com.cpe.backend.controller;
+
+import com.cpe.backend.Entity.PaymentChannel;
+import com.cpe.backend.repository.PaymentChannelRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:8080")
+@RestController
+public class PaymentChannelController {
+
+    @Autowired
+    private final PaymentChannelRepository paymentChannelRepository;
+
+    public PaymentChannelController(PaymentChannelRepository paymentChannelRepository) {
+        this.paymentChannelRepository = paymentChannelRepository;
+    }
+
+    @GetMapping("/paymentChannel")
+    public Collection<PaymentChannel> paymentChannels() {
+        return paymentChannelRepository.findAll().stream().collect(Collectors.toList());
+    }
+
+}
