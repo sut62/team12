37fd7c7fd5f574564ef9afe3448182aa673d03a7
tanapon-pc <tanatoptig555@hotmail.com>
2020-01-17 04:36:54 +0700

    close #111 - แก้ไขข้อมูลใน combobox และปรับปรุงแก้ไข Frontend ของระบบบันทึกข้อมูลเภสัชกร

--- backend/src/main/java/com/cpe/backend/controller/PharmacistController.java
+++ backend/src/main/java/com/cpe/backend/controller/PharmacistController.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 import java.sql.Timestamp;
 import java.sql.Date;
 import java.util.Collection;
-//import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
