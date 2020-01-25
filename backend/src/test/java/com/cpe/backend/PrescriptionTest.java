c6a1ab72b2cb2587780ccfa7e2c52eb521b1d52b
SAROCHA2540 <ming.sarocha.2540@gmail.com>
2020-01-26 02:48:33 +0700

    close #139 - update TestCase ของระบบพิมพ์ใบสั่งยา

--- backend/src/test/java/com/cpe/backend/PrescriptionTest.java
+++ backend/src/test/java/com/cpe/backend/PrescriptionTest.java
@@ -51,55 +51,33 @@ public class PrescriptionTest {
     @Test
     void B5905713_testPrescriptionComplete() {
         Prescription prescription = new Prescription();
-        TitleName titleName = titleNameRepository.findById(5);
-        Doctor doctor_name = doctorRepository.findById(1);
-        Drug drug = drugRepository.findById(1);
-        Unit_of_medicine unit = unit_of_medicineRepository.findById(2);
 
         prescription.setPatient_ID("HN12345678");
-        prescription.setTitleName(titleName);
         prescription.setName("สโรชา สุขสบาย");
         prescription.setAge(22);
         prescription.setDrugallergy("not");
-        prescription.setDoctor_name(doctor_name);
-        prescription.setDrug(drug);
         prescription.setAmount(10);
-        prescription.setUnit_of_medicine(unit);
 
         prescription = prescriptionRepository.saveAndFlush(prescription);
 
         Optional<Prescription> found = prescriptionRepository.findById(prescription.getId());
         assertEquals("HN12345678", found.get().getPatient_ID());
-        assertEquals(titleName, found.get().getTitleName());
         assertEquals("สโรชา สุขสบาย", found.get().getName());
         assertEquals(22, found.get().getAge());
         assertEquals("not", found.get().getDrugallergy());
-        assertEquals(doctor_name, found.get().getDoctor_name());
-        assertEquals(drug, found.get().getDrug());
         assertEquals(10, found.get().getAmount());
-        assertEquals(unit, found.get().getUnit_of_medicine());
     }
     //กรณี Patient_ID ต้องไม่เป็นค่าว่าง
     @Test
     void B5905713_testPatient_IDMustNotBeNull() {
 
         Prescription prescription = new Prescription();
-        TitleName titleName = titleNameRepository.findById(5);
-        Doctor doctor_name = doctorRepository.findById(1);
-        Drug drug = drugRepository.findById(1);
-        Unit_of_medicine unit = unit_of_medicineRepository.findById(2);
-        
         try {
             prescription.setPatient_ID(null);
-            prescription.setTitleName(titleName);
             prescription.setName("สโรชา สุขสบาย");
             prescription.setAge(22);
             prescription.setDrugallergy("not");
-            prescription.setDoctor_name(doctor_name);
-            prescription.setDrug(drug);
             prescription.setAmount(10);
-            prescription.setUnit_of_medicine(unit);
-
         } catch (ConstraintViolationException e) {
 
             Set<ConstraintViolation<Prescription>> result = validator.validate(prescription);
@@ -118,21 +96,12 @@ public class PrescriptionTest {
     @Test
     void B5905713_testPatient_IDFirstCharacterA(){
         Prescription prescription = new Prescription();
-        TitleName titleName = titleNameRepository.findById(5);
-        Doctor doctor_name = doctorRepository.findById(1);
-        Drug drug = drugRepository.findById(1);
-        Unit_of_medicine unit = unit_of_medicineRepository.findById(2);
         try {
             prescription.setPatient_ID("AN12345678");
-            prescription.setTitleName(titleName);
             prescription.setName("สโรชา สุขสบาย");
             prescription.setAge(22);
             prescription.setDrugallergy("not");
-            prescription.setDoctor_name(doctor_name);
-            prescription.setDrug(drug);
             prescription.setAmount(10);
-            prescription.setUnit_of_medicine(unit);
-
         }catch (ConstraintViolationException e) {
 
             Set<ConstraintViolation<Prescription>> result = validator.validate(prescription);
@@ -151,20 +120,13 @@ public class PrescriptionTest {
     @Test
     void B5905713_testPatient_IDSecondCharacterB(){
         Prescription prescription = new Prescription();
-        TitleName titleName = titleNameRepository.findById(5);
-        Doctor doctor_name = doctorRepository.findById(1);
-        Drug drug = drugRepository.findById(1);
-        Unit_of_medicine unit = unit_of_medicineRepository.findById(2);
+        
         try {
             prescription.setPatient_ID("HB12345678");
-            prescription.setTitleName(titleName);
             prescription.setName("สโรชา สุขสบาย");
             prescription.setAge(22);
             prescription.setDrugallergy("not");
-            prescription.setDoctor_name(doctor_name);
-            prescription.setDrug(drug);
             prescription.setAmount(10);
-            prescription.setUnit_of_medicine(unit);
 
         }catch (ConstraintViolationException e) {
 
@@ -184,22 +146,13 @@ public class PrescriptionTest {
     @Test
     void B5905713_testNameMustNotBeNull(){
         Prescription prescription = new Prescription();
-        TitleName titleName = titleNameRepository.findById(5);
-        Doctor doctor_name = doctorRepository.findById(1);
-        Drug drug = drugRepository.findById(1);
-        Unit_of_medicine unit = unit_of_medicineRepository.findById(2);
         
         try {
             prescription.setPatient_ID("HN12345678");
-            prescription.setTitleName(titleName);
             prescription.setName(null);
             prescription.setAge(22);
             prescription.setDrugallergy("not");            
-            prescription.setDoctor_name(doctor_name);
-            prescription.setDrug(drug);
             prescription.setAmount(10);
-            prescription.setUnit_of_medicine(unit);
-
         } catch (ConstraintViolationException e) {
 
             Set<ConstraintViolation<Prescription>> result = validator.validate(prescription);
@@ -218,21 +171,12 @@ public class PrescriptionTest {
     @Test
     void B5905713_testNameSizeLassthan5(){
         Prescription prescription = new Prescription();
-        TitleName titleName = titleNameRepository.findById(5);
-        Doctor doctor_name = doctorRepository.findById(1);
-        Drug drug = drugRepository.findById(1);
-        Unit_of_medicine unit = unit_of_medicineRepository.findById(2);
         try {
             prescription.setPatient_ID("HB12345678");
-            prescription.setTitleName(titleName);
             prescription.setName("มี D");
             prescription.setAge(22);
             prescription.setDrugallergy("not");
-            prescription.setDoctor_name(doctor_name);
-            prescription.setDrug(drug);
             prescription.setAmount(10);
-            prescription.setUnit_of_medicine(unit);
-
         }catch (ConstraintViolationException e) {
 
             Set<ConstraintViolation<Prescription>> result = validator.validate(prescription);
@@ -250,20 +194,12 @@ public class PrescriptionTest {
     @Test
     void B5905713_testNameSizeMorethan50(){
         Prescription prescription = new Prescription();
-        TitleName titleName = titleNameRepository.findById(5);
-        Doctor doctor_name = doctorRepository.findById(1);
-        Drug drug = drugRepository.findById(1);
-        Unit_of_medicine unit = unit_of_medicineRepository.findById(2);
         try {
             prescription.setPatient_ID("HB12345678");
-            prescription.setTitleName(titleName);
             prescription.setName("โรงพยาบาลมหาวิทยาลัยเทคโนโลยีสุรนารี นครราชสีมาาาาา");
             prescription.setAge(22);
             prescription.setDrugallergy("not");
-            prescription.setDoctor_name(doctor_name);
-            prescription.setDrug(drug);
             prescription.setAmount(10);
-            prescription.setUnit_of_medicine(unit);
 
         }catch (ConstraintViolationException e) {
 
@@ -282,21 +218,13 @@ public class PrescriptionTest {
     @Test
     void B5905713_testAgeMustNotBeNull(){
         Prescription prescription = new Prescription();
-        TitleName titleName = titleNameRepository.findById(5);
-        Doctor doctor_name = doctorRepository.findById(1);
-        Drug drug = drugRepository.findById(1);
-        Unit_of_medicine unit = unit_of_medicineRepository.findById(2);
         
         try {
             prescription.setPatient_ID("HN12345678");
-            prescription.setTitleName(titleName);
             prescription.setName("สโรชา สุขสบาย");
             prescription.setAge(null);
             prescription.setDrugallergy("not");
-            prescription.setDoctor_name(doctor_name);
-            prescription.setDrug(drug);
             prescription.setAmount(10);
-            prescription.setUnit_of_medicine(unit);
 
         } catch (ConstraintViolationException e) {
 
@@ -316,21 +244,13 @@ public class PrescriptionTest {
     @Test
     void B5905713_testAgeMin1(){
         Prescription prescription = new Prescription();
-        TitleName titleName = titleNameRepository.findById(5);
-        Doctor doctor_name = doctorRepository.findById(1);
-        Drug drug = drugRepository.findById(1);
-        Unit_of_medicine unit = unit_of_medicineRepository.findById(2);
         
         try {
             prescription.setPatient_ID("HN12345678");
-            prescription.setTitleName(titleName);
             prescription.setName("สโรชา สุขสบาย");
             prescription.setAge(0);
             prescription.setDrugallergy("not");
-            prescription.setDoctor_name(doctor_name);
-            prescription.setDrug(drug);
             prescription.setAmount(10);
-            prescription.setUnit_of_medicine(unit);
 
         } catch (ConstraintViolationException e) {
 
@@ -350,22 +270,12 @@ public class PrescriptionTest {
     @Test
     void B5905713_testAgeMax122(){
         Prescription prescription = new Prescription();
-        TitleName titleName = titleNameRepository.findById(5);
-        Doctor doctor_name = doctorRepository.findById(1);
-        Drug drug = drugRepository.findById(1);
-        Unit_of_medicine unit = unit_of_medicineRepository.findById(2);
-        
         try {
             prescription.setPatient_ID("HN12345678");
-            prescription.setTitleName(titleName);
             prescription.setName("สโรชา สุขสบาย");
             prescription.setAge(123);
             prescription.setDrugallergy("not");
-            prescription.setDoctor_name(doctor_name);
-            prescription.setDrug(drug);
             prescription.setAmount(10);
-            prescription.setUnit_of_medicine(unit);
-
         } catch (ConstraintViolationException e) {
 
             Set<ConstraintViolation<Prescription>> result = validator.validate(prescription);
@@ -384,21 +294,13 @@ public class PrescriptionTest {
     @Test
     void B5905713_testDrugallergyMustNotBeNull(){
         Prescription prescription = new Prescription();
-        TitleName titleName = titleNameRepository.findById(5);
-        Doctor doctor_name = doctorRepository.findById(1);
-        Drug drug = drugRepository.findById(1);
-        Unit_of_medicine unit = unit_of_medicineRepository.findById(2);
         
         try {
             prescription.setPatient_ID("HN12345678");
-            prescription.setTitleName(titleName);
             prescription.setName("สโรชา สุขสบาย");
             prescription.setAge(22);
             prescription.setDrugallergy(null);
-            prescription.setDoctor_name(doctor_name);
-            prescription.setDrug(drug);
             prescription.setAmount(10);
-            prescription.setUnit_of_medicine(unit);
 
         } catch (ConstraintViolationException e) {
 
@@ -418,21 +320,13 @@ public class PrescriptionTest {
     @Test
     void B5905713_testDrugallergyMutNotBeNumber(){
         Prescription prescription = new Prescription();
-        TitleName titleName = titleNameRepository.findById(5);
-        Doctor doctor_name = doctorRepository.findById(1);
-        Drug drug = drugRepository.findById(1);
-        Unit_of_medicine unit = unit_of_medicineRepository.findById(2);
         
         try {
             prescription.setPatient_ID("HN12345678");
-            prescription.setTitleName(titleName);
             prescription.setName("สโรชา สุขสบาย");
             prescription.setAge(22);
             prescription.setDrugallergy("paracetamol56");
-            prescription.setDoctor_name(doctor_name);
-            prescription.setDrug(drug);
             prescription.setAmount(10);
-            prescription.setUnit_of_medicine(unit);
 
         } catch (ConstraintViolationException e) {
 
@@ -451,21 +345,13 @@ public class PrescriptionTest {
     @Test
     void B5905713_testDrugallergyMustNotBePattern(){
         Prescription prescription = new Prescription();
-        TitleName titleName = titleNameRepository.findById(5);
-        Doctor doctor_name = doctorRepository.findById(1);
-        Drug drug = drugRepository.findById(1);
-        Unit_of_medicine unit = unit_of_medicineRepository.findById(2);
         
         try {
             prescription.setPatient_ID("HN12345678");
-            prescription.setTitleName(titleName);
             prescription.setName("สโรชา สุขสบาย");
             prescription.setAge(22);
             prescription.setDrugallergy("!@#$%^&*");
-            prescription.setDoctor_name(doctor_name);
-            prescription.setDrug(drug);
             prescription.setAmount(10);
-            prescription.setUnit_of_medicine(unit);
 
         } catch (ConstraintViolationException e) {
 
@@ -484,21 +370,13 @@ public class PrescriptionTest {
     @Test
     void B5905713_testAmountMustNotBeNull(){
         Prescription prescription = new Prescription();
-        TitleName titleName = titleNameRepository.findById(5);
-        Doctor doctor_name = doctorRepository.findById(1);
-        Drug drug = drugRepository.findById(1);
-        Unit_of_medicine unit = unit_of_medicineRepository.findById(2);
         
         try {
             prescription.setPatient_ID("HN12345678");
-            prescription.setTitleName(titleName);
             prescription.setName("สโรชา สุขสบาย");
             prescription.setAge(22);
             prescription.setDrugallergy("not");
-            prescription.setDoctor_name(doctor_name);
-            prescription.setDrug(drug);
             prescription.setAmount(null);
-            prescription.setUnit_of_medicine(unit);
 
         } catch (ConstraintViolationException e) {
 
@@ -517,21 +395,13 @@ public class PrescriptionTest {
     @Test
     void B5905713_testAmountMin1(){
         Prescription prescription = new Prescription();
-        TitleName titleName = titleNameRepository.findById(5);
-        Doctor doctor_name = doctorRepository.findById(1);
-        Drug drug = drugRepository.findById(1);
-        Unit_of_medicine unit = unit_of_medicineRepository.findById(2);
         
         try {
             prescription.setPatient_ID("HN12345678");
-            prescription.setTitleName(titleName);
             prescription.setName("สโรชา สุขสบาย");
             prescription.setAge(22);
             prescription.setDrugallergy("not");
-            prescription.setDoctor_name(doctor_name);
-            prescription.setDrug(drug);
             prescription.setAmount(0);
-            prescription.setUnit_of_medicine(unit);
 
         } catch (ConstraintViolationException e) {
 
@@ -551,21 +421,13 @@ public class PrescriptionTest {
     @Test
     void B5905713_testAmountMax100(){
         Prescription prescription = new Prescription();
-        TitleName titleName = titleNameRepository.findById(5);
-        Doctor doctor_name = doctorRepository.findById(1);
-        Drug drug = drugRepository.findById(1);
-        Unit_of_medicine unit = unit_of_medicineRepository.findById(2);
         
         try {
             prescription.setPatient_ID("HN12345678");
-            prescription.setTitleName(titleName);
             prescription.setName("สโรชา สุขสบาย");
             prescription.setAge(22);
             prescription.setDrugallergy("not");
-            prescription.setDoctor_name(doctor_name);
-            prescription.setDrug(drug);
             prescription.setAmount(101);
-            prescription.setUnit_of_medicine(unit);
 
         } catch (ConstraintViolationException e) {
 
