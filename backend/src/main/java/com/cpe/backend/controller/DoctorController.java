bc885f81739d442c0016da2ca03972d6f09b5afe
SAROCHA2540 <ming.sarocha.2540@gmail.com>
2020-01-26 02:25:17 +0700

    close #140 - update controller ของระบบพิมพ์ใบสั่งยา

--- backend/src/main/java/com/cpe/backend/controller/DoctorController.java
+++ backend/src/main/java/com/cpe/backend/controller/DoctorController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+import org.springframework.web.bind.annotation.PathVariable;
 import java.util.Collection;
 import java.util.stream.Collectors;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -26,5 +28,9 @@ public class DoctorController {
     public Collection<Doctor> disease() {
         return doctorRepository.findAll().stream().collect(Collectors.toList());
     }
-
+    @GetMapping("/doctor/{id}")
+    public  Optional <Doctor> doctor_name(@PathVariable Long id){
+            Optional<Doctor> doctor_name = doctorRepository.findById(id);
+            return doctor_name;
+    }
 }
